35e7060dfa211c49367ed2fd999ba7f81e0b6b55
gaocodecell <1325261393@qq.com>
2025-02-21 13:43:46 +0800

    GKD

--- Distillation/GNNtoMLP/GraphMLP/wisconsin.sh
+++
@@ -1,9 +0,0 @@
-
-
- python trainoverfitting.py --dataset=cora  --train_rate=0.2   --net=GCN
- python trainoverfitting.py --dataset=cora   --train_rate=0.2  --net=FAGCN
- python trainoverfitting.py --dataset=cora  --train_rate=0.2  --net=BernNet
- python trainoverfitting.py --dataset=cora  --train_rate=0.2  --net=CMPGNN
-
-
-
